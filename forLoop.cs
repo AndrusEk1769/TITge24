04ae57e77a919fb51ed1449fbb09f9d6f92a51a1
AndrusEk1769 <Andreasek18@gmail.com>
2024-12-20 09:12:27 +0200

    Add files via upload

---
+++ forLoop.cs
@@ -0,0 +1,20 @@
+﻿namespace forLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int uhuu = 3;
+
+            for (int i = 0; i <= uhuu; i++)
+            {
+                Console.WriteLine("Tere uhuu");
+
+            }   
+            
+            
+
+        }
+    }
+}
