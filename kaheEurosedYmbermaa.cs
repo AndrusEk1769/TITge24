686f8c845dfb03613a316e5a09f5be0625172524
AndrusEk1769 <Andreasek18@gmail.com>
2024-12-20 09:03:18 +0200

    Add files via upload

---
+++ kaheEurosedYmbermaa.cs
@@ -0,0 +1,22 @@
+﻿namespace MaaYmbermoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            double coinDiameter = 25.77; // mm
+
+            double earthRadius = 6371000000; // mm
+
+            double EarthCirumference = 2 * Math.PI * earthRadius;
+
+
+            Console.WriteLine("Maa ymbermoot" + EarthCirumference);
+
+            Console.WriteLine("maa ymber laheb " + EarthCirumference / coinDiameter + " 2 eurost mynti");
+        }
+    }
+}
