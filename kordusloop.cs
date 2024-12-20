fbb1759c6eb8280d56c320eede45e29d93b83b25
AndrusEk1769 <Andreasek18@gmail.com>
2024-12-20 09:40:52 +0200

    Add files via upload

---
+++ kordusloop.cs
@@ -0,0 +1,20 @@
+﻿namespace loopFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta t'rnide arv");
+
+            int n = Int16.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("*");
+            }
+
+
+            Console.WriteLine("Kasutan selle jaoks for loopi");
+          
+        }
+    }
+}
