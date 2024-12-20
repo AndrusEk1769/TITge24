62b626fdc01b1352860d9faa6eea32b45cad72cc
AndrusEk1769 <Andreasek18@gmail.com>
2024-12-20 10:09:25 +0200

    Add files via upload

---
+++ killkoll.cs
@@ -0,0 +1,27 @@
+﻿namespace loopInLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta korduste arv");
+            string n = " kill-koll ";
+            string a = " killadi-koll";
+
+            int arv = int.Parse(Console.ReadLine());
+
+           for (int i = 0; i <= arv; i++)
+            {
+                Console.WriteLine(a);
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.WriteLine(n);
+                }
+            }   
+
+       
+
+        }
+
+    }
+}
