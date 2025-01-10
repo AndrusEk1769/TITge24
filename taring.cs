ab8a06ba0472dd194423e1fbc43494a87153c891
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-10 10:10:21 +0200

    Add files via upload

---
+++ taring.cs
@@ -0,0 +1,38 @@
+﻿namespace taringuMang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 7);
+            
+            switch (randomNumber)   {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    break;
+                case 4:
+                    Console.WriteLine("4");
+                    break;
+                case 5:
+                    Console.WriteLine("5");
+                    break;
+                case 6:
+                    Console.WriteLine("6");
+                    break;
+            }
+
+
+            
+
+        }
+    }
+}
