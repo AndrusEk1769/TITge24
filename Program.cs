04ce9084a736e25473ace970bbddc30e1501aede
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-10 09:23:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta Vokaal");
+            char vokaal = Convert.ToChar(Console.ReadLine());
+
+           
+            switch (vokaal)
+            {
+                case 'a':
+                    Console.WriteLine("Sisestasid a");
+                    break;
+                case 'e':
+                    Console.WriteLine("Sisestasid e");
+                    break;
+                case 'i':
+                    Console.WriteLine("Sisestasid i");
+                    break;
+                case 'o':
+                    Console.WriteLine("Sisestasid o");
+                    break;
+                case 'u':
+                    Console.WriteLine("Sisestasid u");
+                    break;
+                default:
+                    Console.WriteLine("Ei sisestanud vokaali");
+                    break;
+            }
+
+        }
+    }
+    }
+
