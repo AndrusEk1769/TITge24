999c0c829d881d5f29dc7be9a3c7bf77762347d9
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-30 16:49:09 +0200

    Add files via upload

---
+++ too431jan/too431jan/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace too431jan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("Kas soovid ruutu teha?");
+            string a = "ja";
+            string b = "ei";
+            string vastus = Console.ReadLine();
+
+            switch (vastus)
+            {
+                case "ja":
+                    Ruut();
+                    break;
+                case "ei":
+                    Console.WriteLine("Otsustasite mitte ruutu teha");
+                    break;
+                default:
+                    Console.WriteLine("Palun sisestage kas ja voi ei")
+                    ; break;
+
+
+
+
+
+
+            }
+            static void Ruut()
+            {
+
+                int size = 5;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+
+                }
+                
+
+            }
+
+
+        }
+    }
+}
