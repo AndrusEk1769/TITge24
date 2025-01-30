fdceccb944a4ea90638a5eeff4fc0aa502b47b72
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-30 16:07:59 +0200

    Add files via upload

---
+++ Tunnitoo31jan/Tunnitoo31jan/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Tunnitoo31jan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            Console.WriteLine();
+
+            int rows = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+
+            
+
+            for (int i = 1; i <= rows; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                  
+                    Console.Write(currentNumber + " ");
+                    currentNumber++;
+                   
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
