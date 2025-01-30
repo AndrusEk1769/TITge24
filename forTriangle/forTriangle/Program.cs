0d17608774adcfcbf90b0845d6ae084441ab83be
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-30 16:50:58 +0200

    Add files via upload

---
+++ forTriangle/forTriangle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace forTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
