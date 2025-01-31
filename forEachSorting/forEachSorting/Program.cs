41f941b29a9d183a49991f93cac3613c73489973
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-31 11:12:15 +0200

    Add files via upload

---
+++ forEachSorting/forEachSorting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+namespace forEachSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] genders = { "m", "f", "m", "f", "m", "f", "m", "f", "m", "f", "f", "f", "m" };
+            int maleCount = 0;
+            int femaleCount = 0;
+
+            foreach (string gender in genders)
+            {
+                maleCount = string.Compare(gender, "m", StringComparison.OrdinalIgnoreCase) == 0 ? maleCount + 1 : maleCount;
+                
+                femaleCount = string.Compare(gender, "f", StringComparison.OrdinalIgnoreCase) == 0 ? femaleCount + 1 : femaleCount;
+            }
+            Console.WriteLine("Number of females " + femaleCount);
+            Console.WriteLine("Number of males " + maleCount);
+        }
+    }
+
+}
