98b96b973f7d73ebeae120f50127d5d649c86852
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-31 11:25:51 +0200

    Add files via upload

---
+++ switch31jan/switch31jan/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace switch31jan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage tagstatav summa");
+            double summa = double.Parse(Console.ReadLine());
+            double[] raha = { 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 };
+            Console.WriteLine("Tagastatav summa on: ");
+            foreach (double r in raha)
+            {
+                int tagastus = (int)(summa / r);
+                if (tagastus > 0)
+                {
+                    Console.WriteLine(tagastus + " x " + r);
+                    summa -= tagastus * r;
+                }
+            }
+
+        }
+    }
+}
