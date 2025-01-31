153316233d9a1da496717b41573770b20b3c257c
AndrusEk1769 <Andreasek18@gmail.com>
2025-01-31 11:11:44 +0200

    Add files via upload

---
+++ forEachLoop/forEachLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace forEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] myArray = { "H", "e", "l", "l", "o" };
+
+            foreach (string item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
