351f7b2c72ee9a61bf0fff272845e0af08cf9968
AndrusEk1769 <Andreasek18@gmail.com>
2025-02-14 11:41:09 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,148 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+using System;
 
-namespace RandomAsterisk
+namespace ArraySOrtNames
 {
     internal class Program
     {
-        static Random random = new Random();
-
-        static void PrintAsterisks(int num)
-        {
-            Console.WriteLine(new string('*', num));
-        }
         static void Main(string[] args)
         {
-            for (int i = 0; i < 30; i++)
+            string[] animals = { "cat", "alligator", "fox", "donkey", "bear", "elephant", "goat" };
+
+            Array.Sort(animals);
+            Array.ForEach(animals, animal => { Console.WriteLine(animal); });
+
+
+            Console.WriteLine("------------------------------");
+
+            int[] numbers = { 3, 2, 23, 4, 5, 4, 3, 55, 2 };
+            Array.Sort(numbers);
+
+
+            List<int> uniqueList = new List<int>();
+
+
+            foreach (var num in numbers)
             {
-                int randomNumber = random.Next(1, 20);
-                PrintAsterisks(randomNumber);
+
+                if (uniqueList.Count == 0 || uniqueList[uniqueList.Count - 1] != num)
+                {
+                    uniqueList.Add(num);
+                }
             }
 
 
+            int[] sortedUniqueArray = uniqueList.ToArray();
 
 
+            Console.WriteLine("Sorted array with unique numbers:");
+
+            foreach (var num in sortedUniqueArray)
+            {
+                Console.Write(num + " ");
+            }
         }
-
-
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
