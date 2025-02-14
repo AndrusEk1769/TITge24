3c70a0b51eac3480fd43bfc08053a0cecf7c84ae
AndrusEk1769 <Andreasek18@gmail.com>
2025-02-14 10:40:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,35 @@
-﻿namespace numbriteKolmnurk
+﻿using System;
+
+namespace ListMinSumMaxAvg
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sisestage number");
+            Console.WriteLine("Hello World!");
 
-            int rows = int.Parse(Console.ReadLine());
+            int[] numbers = new int[5] { 10, 15, 16, 8, 6 };
 
-            for (int i = 1; i <= rows; i++)
-            {
-                for (int j = 1; j < rows; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write(j + " ");
-                }
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Average());
+            Console.WriteLine(numbers.Sum());
 
-                Console.WriteLine();
-            }
+            Console.WriteLine("------------------------------");
+            Array.Sort(numbers);
+            Array.ForEach(numbers, x => Console.WriteLine(x));
+
+
+            Console.WriteLine("------------------------------");
+            Array.Reverse(numbers);
+            Array.ForEach(numbers, x => Console.WriteLine(x));
+
+
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine(Array.BinarySearch(numbers, 15));
+
         }
     }
 }
